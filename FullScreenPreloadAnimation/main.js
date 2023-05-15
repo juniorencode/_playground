0c6afcdb1c8017d1269ed28631aa0ed4846744d1
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-05-15 15:41:51 -0500

    feat(FullScreenPreloadAnimation): animate counter and progress bar

---
+++ FullScreenPreloadAnimation/main.js
@@ -0,0 +1,33 @@
+const PreloaderFullScreen = () => {
+  const preloader = document.querySelector('.preloader');
+  const preloaderBlock = preloader.querySelector('.preloader__block');
+  const progressNumber = preloaderBlock.querySelector('.preloader__percent');
+  const progressTitle = preloaderBlock.querySelector('.preloader__title');
+  const progressLoading = preloaderBlock.querySelector('.preloader__loading');
+
+  const preloaderBar = preloader.querySelector('.preloader__bar');
+  const preloaderProgress = preloaderBar.querySelector('.preloader__progress');
+
+  const preloaderAfter = preloader.querySelector('.preloader__after');
+  const preloaderBefore = preloader.querySelector('.preloader__before');
+
+  window.addEventListener('load', () => {
+    let val = 0;
+
+    const updateAnimation = () => {
+      val += 0.5;
+      preloaderProgress.style.width = val + '%';
+      let f =
+        (preloaderProgress.clientWidth /
+          preloaderProgress.parentNode.clientWidth) *
+        100;
+      progressNumber.innerText = parseInt(f);
+
+      if (val === 100) {
+        clearInterval(preloadInterval);
+      }
+    };
+
+    const preloadInterval = setInterval(updateAnimation, 1);
+  });
+};
