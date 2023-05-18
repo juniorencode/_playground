ab104bb8e52e517c2bdbf01008a26cce774f39fa
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-05-18 14:36:11 -0500

    feat(ColorPicker): capture color

--- ColorPicker/main.js
+++ ColorPicker/main.js
@@ -3,17 +3,22 @@ const spectrumCtx = spectrumCanvas.getContext('2d');
 const spectrumCursor = document.getElementById('spectrum-cursor');
 const spectrumRect = spectrumCanvas.getBoundingClientRect();
 
-const createLinearGradient = (cv, ctx, color, horizontal = true) => {
+let currentColor = '';
+let hue = 0;
+let saturation = 1;
+let lightness = 0.5;
+
+const createLinearGradient = (canvas, ctx, color, horizontal = true) => {
   const gradient = ctx.createLinearGradient(
     0,
     0,
-    horizontal ? cv.width : 0,
-    !horizontal ? cv.height : 0
+    horizontal ? canvas.width : 0,
+    !horizontal ? canvas.height : 0
   );
   gradient.addColorStop(0, horizontal ? color : 'transparent');
   gradient.addColorStop(1, !horizontal ? color : 'transparent');
   ctx.fillStyle = gradient;
-  ctx.fillRect(0, 0, cv.width, cv.height);
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 };
 
 const createShadeSpectrum = color => {
@@ -23,6 +28,30 @@ const createShadeSpectrum = color => {
 
   createLinearGradient(spectrumCanvas, spectrumCtx, '#fff');
   createLinearGradient(spectrumCanvas, spectrumCtx, '#000', false);
+
+  spectrumCanvas.addEventListener('click', function (e) {
+    getSpectrumColor(e);
+  });
+};
+
+const getSpectrumColor = e => {
+  e.preventDefault();
+
+  const x = e.pageX - spectrumRect.left;
+  const y = e.pageY - spectrumRect.top;
+
+  if (x > spectrumRect.width) x = spectrumRect.width;
+  if (x < 0) x = 0;
+  if (y > spectrumRect.height) y = spectrumRect.height;
+  if (y < 0) y = 0.1;
+
+  const xRatio = (x / spectrumRect.width) * 100;
+  const yRatio = (y / spectrumRect.height) * 100;
+  const hsvValue = 1 - yRatio / 100;
+  const hsvSaturation = xRatio / 100;
+  lightness = (hsvValue / 2) * (2 - hsvSaturation);
+  saturation = (hsvValue * hsvSaturation) / (1 - Math.abs(2 * lightness - 1));
+  console.log('hsl ' + hue + ' ' + saturation + ' ' + lightness);
 };
 
 function ColorPicker() {
