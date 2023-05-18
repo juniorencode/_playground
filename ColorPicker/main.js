924d1bfd8a6cc9b53ef06bac9b82284a72f96bce
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-05-18 14:37:56 -0500

    feat(ColorPicker): convert hsl to rgb

--- ColorPicker/main.js
+++ ColorPicker/main.js
@@ -8,6 +8,46 @@ let hue = 0;
 let saturation = 1;
 let lightness = 0.5;
 
+// convert hue to RGB
+const hueToRgb = (p, q, t) => {
+  if (t < 0) t += 1;
+  if (t > 1) t -= 1;
+  if (t < 1 / 6) return p + (q - p) * 6 * t;
+  if (t < 1 / 2) return q;
+  if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+  return p;
+};
+
+// convert hsl to RGB
+const hslToRgb = (h, s, l) => {
+  // normalize the HSL values to the range [0, 1]
+  h = (h % 360) / 360;
+  s = Math.min(1, Math.max(0, s));
+  l = Math.min(1, Math.max(0, l));
+
+  // calculate the RGB components
+  let r, g, b;
+
+  if (s === 0) {
+    // if the saturation is 0, the color is a grayscale
+    r = g = b = l;
+  } else {
+    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    const p = 2 * l - q;
+
+    r = hueToRgb(p, q, h + 1 / 3);
+    g = hueToRgb(p, q, h);
+    b = hueToRgb(p, q, h - 1 / 3);
+  }
+
+  // scale the RGB components to the range [0, 255]
+  return {
+    r: Math.round(r * 255),
+    g: Math.round(g * 255),
+    b: Math.round(b * 255)
+  };
+};
+
 const createLinearGradient = (canvas, ctx, color, horizontal = true) => {
   const gradient = ctx.createLinearGradient(
     0,
@@ -51,7 +91,8 @@ const getSpectrumColor = e => {
   const hsvSaturation = xRatio / 100;
   lightness = (hsvValue / 2) * (2 - hsvSaturation);
   saturation = (hsvValue * hsvSaturation) / (1 - Math.abs(2 * lightness - 1));
-  console.log('hsl ' + hue + ' ' + saturation + ' ' + lightness);
+  const color = hslToRgb(hue, saturation, lightness);
+  console.log(color);
 };
 
 function ColorPicker() {
