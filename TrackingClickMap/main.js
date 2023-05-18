5b4d11c53cd51d68f63774fe299aaca90bd57a8b
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-05-17 19:08:42 -0500

    feat(TrackingClickMap): add density gradient

--- TrackingClickMap/main.js
+++ TrackingClickMap/main.js
@@ -4,44 +4,54 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const pointSize = 4;
 const points = [];
 
-// Constants for the KDE
+// constants for the KDE
 const gridSize = 1; // grid size
-const kernelBandwidth = 30; // kernel bandwidth
+const kernelBandwidth = 20; // kernel bandwidth
+const bandwidthSquared = 2 * kernelBandwidth * kernelBandwidth;
 
-const drawMap = (x, y) => {
+// define reference colors
+var colors = [
+  [0, 0, 0], // inverted white to black
+  [255, 255, 0], // inverted blue to yellow
+  [255, 0, 0], // inverted cyan to red
+  [255, 0, 255], // inverted green to magenta
+  [0, 0, 255], // inverted yellow to blue
+  [0, 255, 255] // inverted red to cyan
+];
+
+const drawMap = () => {
   clear();
 
-  // generate points
-  points.push({ x: x, y: y });
-
   // calculate density using KDE
-  let totalDensity = 0; // total sum of densities
+  const pointsLength = points.length;
+  let maxDensity = 0; // maximum density
+  let rango = 80; // radial range
 
-  for (let j = 0; j < points.length; j++) {
-    const distX = x - points[j].x;
-    const distY = y - points[j].y;
-    const distSquared = distX * distX + distY * distY;
+  for (let j = 0; j < pointsLength; j++) {
+    let density = 0;
 
-    totalDensity += Math.exp(
-      -distSquared / (5 * kernelBandwidth * kernelBandwidth)
-    );
+    for (let k = 0; k < pointsLength; k++) {
+      const distX = points[k].x - points[j].x;
+      const distY = points[k].y - points[j].y;
+      const distSquared = distX * distX + distY * distY;
+      density += Math.exp(-distSquared / bandwidthSquared);
+    }
+
+    maxDensity = Math.max(maxDensity, density);
   }
 
   for (let x = 0; x < canvas.width; x += gridSize) {
     for (let y = 0; y < canvas.height; y += gridSize) {
       let density = 0;
 
-      for (let j = 0; j < points.length; j++) {
+      for (let j = 0; j < pointsLength; j++) {
         const distX = x - points[j].x;
         const distY = y - points[j].y;
         const distSquared = distX * distX + distY * distY;
 
-        density += Math.exp(
-          -distSquared / (2 * kernelBandwidth * kernelBandwidth)
-        );
+        density += Math.exp(-distSquared / bandwidthSquared);
       }
 
       // draw density on the canvas
@@ -49,27 +59,40 @@ const drawMap = (x, y) => {
       ctx.rect(x, y, gridSize, gridSize);
 
       // adjust color based on density and total sum of densities
-      const colorIntensity = density / totalDensity;
-      const color = 'rgba(0, 0, 255, ' + colorIntensity + ')';
-      ctx.fillStyle = color;
+      const colorIntensity = density / maxDensity;
+
+      // calculate color index based on intensity
+      var colorIndex = Math.floor(colorIntensity * (colors.length - 1));
+
+      // get adjacent colors
+      var lowerColor = colors[colorIndex];
+      var upperColor = colors[colorIndex + 1];
+
+      // check if we are in the last color
+      if (!upperColor) {
+        ctx.fillStyle =
+          'rgba(' +
+          lowerColor[0] +
+          ', ' +
+          lowerColor[1] +
+          ', ' +
+          lowerColor[2] +
+          ', 1)';
+      } else {
+        // calculate interpolation fraction
+        var fraction = colorIntensity * (colors.length - 1) - colorIndex;
+
+        // perform linear interpolation between the colors
+        var red = interpolate(lowerColor[0], upperColor[0], fraction);
+        var green = interpolate(lowerColor[1], upperColor[1], fraction);
+        var blue = interpolate(lowerColor[2], upperColor[2], fraction);
+
+        var color = 'rgba(' + red + ', ' + green + ', ' + blue + ', 1)';
+        ctx.fillStyle = color;
+      }
       ctx.fill();
       ctx.closePath();
     }
-  }
-
-  // draw red points on top of the gradient
-  for (let i = 0; i < points.length; i++) {
-    const point = points[i];
-    ctx.beginPath();
-    ctx.rect(
-      point.x - pointSize / 2,
-      point.y - pointSize / 2,
-      pointSize,
-      pointSize
-    );
-    ctx.fillStyle = 'red';
-    ctx.fill();
-    ctx.closePath();
   }
 };
 
@@ -78,6 +101,14 @@ const clear = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 };
 
+// linear interpolation function
+function interpolate(a, b, fraction) {
+  return Math.round(a + (b - a) * fraction);
+}
+
 document.body.addEventListener('click', e => {
-  drawMap(e.clientX, e.clientY);
+  // generate point
+  points.push({ x: e.clientX, y: e.clientY });
+
+  drawMap();
 });
