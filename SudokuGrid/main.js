565b2661ec36b29d8e54f42000dee9012104e0d8
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-05-19 18:44:10 -0500

    feat(SudokuGrid): draw matches

--- SudokuGrid/main.js
+++ SudokuGrid/main.js
@@ -1,14 +1,13 @@
-// initial Sudoku Configuration
 const board = [
-  [5, 3, 4, 6, 7, 8, 9, 1, 2],
-  [6, 7, 2, 1, 9, 5, 3, 4, 8],
-  [1, 9, 8, 3, 4, 2, 5, 6, 7],
-  [8, 5, 9, 7, 6, 1, 4, 2, 3],
-  [4, 2, 6, 8, 5, 3, 7, 9, 1],
-  [7, 1, 3, 9, 2, 4, 8, 5, 6],
-  [9, 6, 1, 5, 3, 7, 2, 8, 4],
-  [2, 8, 7, 4, 1, 9, 6, 3, 5],
-  [3, 4, 5, 2, 8, 6, 1, 7, 9]
+  ['5', '', '4', '6', '', '8', '', '1', '2'],
+  ['6', '7', '2', '', '9', '', '3', '4', '8'],
+  ['1', '9', '8', '3', '4', '2', '', '', ''],
+  ['8', '', '9', '7', '6', '1', '', '2', ''],
+  ['4', '', '6', '', '', '', '7', '9', '1'],
+  ['7', '1', '', '', '', '4', '8', '5', ''],
+  ['', '6', '1', '', '3', '7', '', '', ''],
+  ['2', '', '', '4', '', '', '6', '', '5'],
+  ['', '', '', '', '', '6', '1', '7', '9']
 ];
 
 const canvas = document.querySelector('#sudoku');
@@ -43,7 +42,23 @@ canvas.addEventListener('click', e => {
   selectedNumber = board[selectedRow][selectedCol];
 
   console.log(selectedNumber);
+
+  drawSudoku();
 });
+
+const drawSelectedCell = cellSize => {
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      const number = board[row][col];
+
+      // Dibujar el cuadro resaltado si está seleccionado o si el número coincide
+      if (number !== '' && number === selectedNumber) {
+        ctx.fillStyle = '#bbdefb';
+        ctx.fillRect(col * cellSize, row * cellSize, cellSize, cellSize);
+      }
+    }
+  }
+};
 
 const drawGrid = (cellSize, canvasSize) => {
   ctx.strokeStyle = 'lightgray';
@@ -101,6 +116,7 @@ const drawSudoku = () => {
 
   ctx.clearRect(0, 0, canvasSize, canvasSize);
 
+  drawSelectedCell(cellSize);
   drawGrid(cellSize, canvasSize);
   drawNumber(cellSize);
 };
