18fee63cedd8bf69efe1e92762fb3041f092a8ca
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-05-23 21:18:36 -0500

    feat(ResponsiveSlider): move right

---
+++ ResponsiveSlider/main.js
@@ -0,0 +1,32 @@
+const slider = document.querySelector('#slider');
+const container = slider.querySelector('#container');
+const slides = slider.querySelectorAll('.slider__slide');
+const arrLeft = slider.querySelector('#arrow-left');
+const arrRight = slider.querySelector('#arrow-right');
+
+let offset = 0;
+let slideIncrement = 0;
+let slideDecrement = slides.length - 1;
+
+arrRight.addEventListener('click', () => {
+  arrRight.disabled = true;
+  offset = slides[0].offsetWidth;
+  container.style.transition = 'left ease-in-out 500ms';
+  container.style.left = -offset + 'px';
+
+  setTimeout(() => {
+    container.style.transition = 'none';
+    slides[slideIncrement].style.order = slides.length - 1;
+    container.style.left = 0;
+    slideIncrement++;
+    slideDecrement = slideIncrement - 1;
+
+    if (slideIncrement === slides.length) {
+      slideIncrement = 0;
+      slides.forEach(slide => {
+        slide.style.order = 'initial';
+      });
+    }
+    arrRight.disabled = false;
+  }, 500);
+});
