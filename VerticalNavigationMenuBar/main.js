bae36c3ec0fdd7a4b1963df6bf178a3c5f74f6d4
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-05-25 11:47:47 -0500

    feat(VerticalNavigationMenuBar): menu show animation

--- VerticalNavigationMenuBar/main.js
+++ VerticalNavigationMenuBar/main.js
@@ -1,5 +1,7 @@
+const container = document.querySelector('.container');
 const toggleClick = document.querySelector('.toggle-box');
 
 toggleClick.addEventListener('click', () => {
   toggleClick.classList.toggle('active');
+  container.classList.toggle('active');
 });
