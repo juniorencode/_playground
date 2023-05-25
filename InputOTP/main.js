f3763bd76a957a8610064fdfaf55fdac7b30b07d
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-05-25 09:02:06 -0500

    feat(InputOTP): add filled behavior

--- InputOTP/main.js
+++ InputOTP/main.js
@@ -1,6 +1,15 @@
 const inputs = document.querySelectorAll('.otp__input');
 
+const toggleFilledClass = field => {
+  if (field.value) {
+    field.classList.add('otp__input--filled');
+  } else {
+    field.classList.remove('otp__input--filled');
+  }
+};
+
 inputs.forEach(input => {
+  toggleFilledClass(input);
   // backspace event
   input.addEventListener('keydown', e => {
     e.preventDefault();
@@ -11,12 +20,33 @@ inputs.forEach(input => {
     } else {
       if (!/^[a-zA-Z0-9]$/.test(e.key) || e.key.length !== 1) return;
 
-      if (!input.value && input.nextElementSibling) {
+      if (input.nextElementSibling) {
         input.value = e.key;
         input.nextElementSibling.focus();
       } else if (inputs[inputs.length - 1] === input) {
         input.value = e.key;
       }
     }
+    toggleFilledClass(input);
   });
 });
+
+// inputs.forEach((input, currentIndex) => {
+//   // fill check
+//   toggleFilledClass(input);
+
+//   // paste event
+//   input.addEventListener('paste', e => {
+//     e.preventDefault();
+//     const text = e.clipboardData.getData('text');
+//     console.log(text);
+//     inputs.forEach((item, index) => {
+//       if (index >= currentIndex && text[index - currentIndex]) {
+//         item.focus();
+//         item.value = text[index - currentIndex] || '';
+//         toggleFilledClass(item);
+//         verifyOTP();
+//       }
+//     });
+//   });
+// });
