9d01b3a19d99ad00494f4f3bda288f7454db4763
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-05-25 09:32:37 -0500

    feat(InputOTP): handle paste

--- InputOTP/main.js
+++ InputOTP/main.js
@@ -1,5 +1,51 @@
 const inputs = document.querySelectorAll('.otp__input');
 
+inputs.forEach((input, currentIndex) => {
+  // paste event
+  input.addEventListener('paste', e => {
+    e.preventDefault();
+    const text = e.clipboardData.getData('text').replace(/[^a-zA-Z0-9\s]/g, '');
+    console.log(text);
+    inputs.forEach((item, index) => {
+      if (index >= currentIndex && text[index - currentIndex]) {
+        item.value = text[index - currentIndex] || '';
+
+        if (item.nextElementSibling) {
+          item.nextElementSibling.focus();
+        } else {
+          item.blur();
+        }
+
+        toggleFilledClass(item);
+      }
+    });
+  });
+
+  // backspace event
+  input.addEventListener('keydown', e => {
+    if (e.ctrlKey) return;
+
+    e.preventDefault();
+
+    if (e.key === 'Backspace') {
+      input.value = '';
+      if (input.previousElementSibling) input.previousElementSibling.focus();
+    } else {
+      if (!/^[a-zA-Z0-9]$/.test(e.key) || e.key.length !== 1) return;
+
+      input.value = e.key;
+
+      if (input.nextElementSibling) {
+        input.nextElementSibling.focus();
+      } else {
+        input.blur();
+      }
+    }
+
+    toggleFilledClass(input);
+  });
+});
+
 const toggleFilledClass = field => {
   if (field.value) {
     field.classList.add('otp__input--filled');
@@ -7,46 +53,3 @@ const toggleFilledClass = field => {
     field.classList.remove('otp__input--filled');
   }
 };
-
-inputs.forEach(input => {
-  toggleFilledClass(input);
-  // backspace event
-  input.addEventListener('keydown', e => {
-    e.preventDefault();
-    if (e.key === 'Backspace') {
-      input.value = '';
-
-      if (input.previousElementSibling) input.previousElementSibling.focus();
-    } else {
-      if (!/^[a-zA-Z0-9]$/.test(e.key) || e.key.length !== 1) return;
-
-      if (input.nextElementSibling) {
-        input.value = e.key;
-        input.nextElementSibling.focus();
-      } else if (inputs[inputs.length - 1] === input) {
-        input.value = e.key;
-      }
-    }
-    toggleFilledClass(input);
-  });
-});
-
-// inputs.forEach((input, currentIndex) => {
-//   // fill check
-//   toggleFilledClass(input);
-
-//   // paste event
-//   input.addEventListener('paste', e => {
-//     e.preventDefault();
-//     const text = e.clipboardData.getData('text');
-//     console.log(text);
-//     inputs.forEach((item, index) => {
-//       if (index >= currentIndex && text[index - currentIndex]) {
-//         item.focus();
-//         item.value = text[index - currentIndex] || '';
-//         toggleFilledClass(item);
-//         verifyOTP();
-//       }
-//     });
-//   });
-// });
