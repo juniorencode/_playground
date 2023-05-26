8f3e672b85c83d5e3688c816c237ea99d409d566
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-05-26 13:00:55 -0500

    feat(EnglishVerbPractice): check correct words

--- EnglishVerbPractice/main.js
+++ EnglishVerbPractice/main.js
@@ -3,10 +3,12 @@ const baseForm = document.querySelector('#base-form');
 const thirdPerson = document.querySelector('#third-person');
 const past = document.querySelector('#past');
 const participle = document.querySelector('#participle');
+let selected = {};
+let checked = {};
 
 const play = () => {
-  const selected = selectVerb();
-  printVerb(selected);
+  reset();
+  printVerb();
 };
 
 const selectVerb = () => {
@@ -14,7 +16,7 @@ const selectVerb = () => {
   return verbs[number];
 };
 
-const printVerb = selected => {
+const printVerb = () => {
   spanish.innerText = selected.spanish;
   baseForm.nextElementSibling.innerText = selected.base;
   gerund.nextElementSibling.innerText = selected.gerund;
@@ -22,5 +24,67 @@ const printVerb = selected => {
   past.nextElementSibling.innerText = selected.past;
   participle.nextElementSibling.innerText = selected.participle;
 };
+
+const check = (e, type) => {
+  if (e.target.value === '') return;
+
+  const elem = e.target;
+  const value = elem.value;
+
+  elem.classList.remove('good');
+  elem.classList.remove('wrong');
+
+  if (value === selected[type]) {
+    checked[type] = true;
+    elem.classList.add('good');
+  } else {
+    elem.classList.add('wrong');
+  }
+
+  isComplete() && play();
+};
+
+const isComplete = () => {
+  return Object.values(checked).every(elem => elem === true);
+};
+
+const reset = () => {
+  selected = selectVerb();
+  checked = {
+    base: false,
+    gerund: false,
+    third: false,
+    past: false,
+    participle: false
+  };
+
+  baseForm.value = '';
+  baseForm.classList.remove('good');
+  baseForm.classList.remove('wrong');
+
+  gerund.value = '';
+  gerund.classList.remove('good');
+  gerund.classList.remove('wrong');
+
+  thirdPerson.value = '';
+  thirdPerson.classList.remove('good');
+  thirdPerson.classList.remove('wrong');
+
+  past.value = '';
+  past.classList.remove('good');
+  past.classList.remove('wrong');
+
+  participle.value = '';
+  participle.classList.remove('good');
+  participle.classList.remove('wrong');
+
+  baseForm.focus();
+};
+
+baseForm.addEventListener('blur', e => check(e, 'base'));
+gerund.addEventListener('blur', e => check(e, 'gerund'));
+thirdPerson.addEventListener('blur', e => check(e, 'third'));
+past.addEventListener('blur', e => check(e, 'past'));
+participle.addEventListener('blur', e => check(e, 'participle'));
 
 play();
