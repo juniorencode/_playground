aa4edad01cc0a19ecdd92617ff8623968b008d11
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-05-27 14:55:28 -0500

    feat(ChartGenerate): calculate stadictic data

--- ChartGenerate/main.js
+++ ChartGenerate/main.js
@@ -1,14 +1,55 @@
 class Chart {
-  constructor(canvas) {
+  constructor(canvas, options) {
     this.canvas = canvas;
     this.ctx = this.canvas.getContext('2d');
     this.ratio = { w: 2, h: 1 };
+    this.labels = options.data.labels;
+    this.data = options.data.datasets[0].data;
 
-    // normalize size
-    this.canvas.width = this.canvas.parentNode.clientWidth;
-    this.canvas.height = this.canvas.width / 2;
+    this.maxValue = Math.max(...this.data);
+    this.minValue = Math.min(...this.data);
+    this.numLabels = 10;
+    this.range = 0;
+    this.calculateStadistic();
+  }
 
-    console.log(this.canvas.width);
-    console.log(this.canvas.height);
+  calculateStadistic() {
+    // adjust the minimum value if it is less than zero
+    const adjustedMin = Math.min(0, this.minValue);
+
+    // // calculate the range of values
+    this.range = this.maxValue - adjustedMin;
+
+    // // calculate the width of each interval
+    this.intervalWidth = this.calculateIntervalWidth(
+      this.range,
+      this.numLabels
+    );
+    console.log(this.intervalWidth);
+
+    // // round the range limits down and up
+    this.roundedMin =
+      Math.floor(adjustedMin / this.intervalWidth) * this.intervalWidth;
+    this.roundedMax =
+      Math.ceil(this.maxValue / this.intervalWidth) * this.intervalWidth;
+  }
+
+  calculateIntervalWidth(range, numLabels) {
+    const intervalWidth = Math.ceil(range / numLabels);
+    const multiple = this.nearestPowerOfTen(intervalWidth);
+    let newIntervalWidth = intervalWidth;
+
+    while (newIntervalWidth % multiple !== 0) {
+      newIntervalWidth++;
+    }
+
+    return newIntervalWidth;
+  }
+
+  nearestPowerOfTen(number) {
+    const log = Math.floor(Math.log10(number)); // calculate the logarithm base 10
+    const powerOfTen = Math.pow(10, log); // calculate the power of ten
+
+    return powerOfTen;
   }
 }
