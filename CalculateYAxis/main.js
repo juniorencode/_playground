a58a22216b845c743a3b08c6baa2106540645373
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-05-27 02:10:55 -0500

    feat(CalculateYAxis): add specific case numbers less 10

--- CalculateYAxis/main.js
+++ CalculateYAxis/main.js
@@ -6,12 +6,12 @@ const isCleanNumber = number => {
   return Math.abs(number) % 10 === 0;
 };
 
-const findCleanNumbers = elem => {
+const findCleanNumbers = (elem, low = false) => {
   const ini = elem.minValue < 0 ? -elem.absMax : 0;
   const cleanNumbers = [];
 
   for (let i = ini; i <= elem.absMax; i++) {
-    if (isCleanNumber(i)) {
+    if (isCleanNumber(i) || low) {
       cleanNumbers.push(i);
     }
   }
@@ -19,44 +19,78 @@ const findCleanNumbers = elem => {
   return cleanNumbers;
 };
 
-const reduceToNearestPowerOfTen = number => {
-  const log = Math.floor(Math.log10(number));
-  const powerOfTen = Math.pow(10, log);
-  const reduced = Math.ceil(number / powerOfTen) * powerOfTen;
-
-  return reduced;
-};
-
 const calculate = elem => {
-  elem.step = 10;
-  elem.absMinValue = Math.floor(elem.minValue / elem.step) * elem.step;
-  elem.absMaxValue = Math.ceil(elem.maxValue / elem.step) * elem.step;
-  elem.absMax = Math.max(
-    Math.abs(elem.absMinValue),
-    Math.abs(elem.absMaxValue)
-  );
-  elem.absMax =
-    elem.absMax % (elem.step * 2) !== 0 ? elem.absMax + elem.step : elem.absMax;
-  elem.cleanNumbers = findCleanNumbers(elem);
-  elem.result = elem.cleanNumbers.filter(
-    n => n >= elem.absMinValue - elem.step && n <= elem.absMaxValue + elem.step
-  );
-
-  while (elem.cleanNumbers.length > elem.limit) {
-    elem.step *= 2;
-    const tempCleanNumbers = [];
-
-    for (let i = 0; i < elem.cleanNumbers.length; i++) {
-      if (i % 2 === 0 || Math.abs(elem.cleanNumbers[i]) === 0) {
-        tempCleanNumbers.push(elem.cleanNumbers[i]);
-      }
-    }
-
-    elem.cleanNumbers = tempCleanNumbers;
+  if (Math.max(Math.abs(elem.minValue), Math.abs(elem.maxValue)) >= 10) {
+    elem.step = 10;
+    elem.absMinValue = Math.floor(elem.minValue / elem.step) * elem.step;
+    elem.absMaxValue = Math.ceil(elem.maxValue / elem.step) * elem.step;
+    elem.absMax = Math.max(
+      Math.abs(elem.absMinValue),
+      Math.abs(elem.absMaxValue)
+    );
+    elem.absMax =
+      elem.absMax % (elem.step * 2) !== 0
+        ? elem.absMax + elem.step
+        : elem.absMax;
+    elem.cleanNumbers = findCleanNumbers(elem);
     elem.result = elem.cleanNumbers.filter(
       n =>
         n >= elem.absMinValue - elem.step && n <= elem.absMaxValue + elem.step
     );
+
+    while (elem.cleanNumbers.length > elem.limit) {
+      elem.step *= 2;
+      const tempCleanNumbers = [];
+
+      for (let i = 0; i < elem.cleanNumbers.length; i++) {
+        if (i % 2 === 0 || Math.abs(elem.cleanNumbers[i]) === 0) {
+          tempCleanNumbers.push(elem.cleanNumbers[i]);
+        }
+      }
+
+      elem.cleanNumbers = tempCleanNumbers;
+      elem.result = elem.cleanNumbers.filter(
+        n =>
+          n >= elem.absMinValue - elem.step && n <= elem.absMaxValue + elem.step
+      );
+    }
+  } else {
+    elem.step = 0;
+    elem.absMinValue = Math.floor(elem.minValue);
+    elem.absMaxValue = Math.ceil(elem.maxValue);
+    elem.absMax = Math.max(
+      Math.abs(elem.absMinValue),
+      Math.abs(elem.absMaxValue)
+    );
+    elem.absMax += elem.absMax % 2 !== 0 ? 1 : 0;
+    elem.cleanNumbers = findCleanNumbers(elem, true);
+    elem.result = elem.cleanNumbers.filter(n => {
+      if (elem.absMinValue > 0) return n - elem.step <= elem.absMaxValue;
+
+      return (
+        n - elem.step >= elem.absMinValue && n - elem.step <= elem.absMaxValue
+      );
+    });
+
+    while (elem.cleanNumbers.length > elem.limit) {
+      const tempCleanNumbers = [];
+
+      for (let i = 0; i < elem.cleanNumbers.length; i++) {
+        if (i % 2 === 0 || Math.abs(elem.cleanNumbers[i]) === 0) {
+          tempCleanNumbers.push(elem.cleanNumbers[i]);
+        }
+      }
+
+      elem.step += 1;
+      elem.cleanNumbers = tempCleanNumbers;
+      elem.result = elem.cleanNumbers.filter(n => {
+        if (elem.absMinValue > 0) return n - elem.step <= elem.absMaxValue;
+
+        return (
+          n - elem.step >= elem.absMinValue && n - elem.step <= elem.absMaxValue
+        );
+      });
+    }
   }
 };
 
@@ -95,9 +129,25 @@ const createCard = (minValue, maxValue, limit) => {
   });
 };
 
+createCard(-9, 9, 5);
+createCard(-1, 1, 5);
+createCard(1, 1, 5);
+createCard(1, 2, 5);
+createCard(2, 3, 5);
+createCard(2, 4, 5);
+createCard(2, 5, 5);
+createCard(2, 6, 5);
+createCard(2, 7, 5);
+createCard(2, 8, 5);
+createCard(2, 9, 5);
+
+createCard(1, 9, 10);
+createCard(1, 9, 5);
+createCard(-8, 6, 10);
+createCard(-5, 5, 5);
+createCard(16, 157, 5);
 createCard(-1200, 700, 11);
 createCard(-44, 182, 11);
 createCard(-44, 192, 11);
-createCard(16, 157, 5);
 
 print();
