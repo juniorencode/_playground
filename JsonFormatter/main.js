319c03bfd1ac2fda6617730e1466aa5d12425d3d
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-06-04 19:14:55 -0500

    feat(JsonFormatter): prefix object and array

--- JsonFormatter/main.js
+++ JsonFormatter/main.js
@@ -1,11 +1,11 @@
-const toggables = document.querySelectorAll('.JsonFormatter__entry--toggable');
+const toggables = document.querySelectorAll('.JFormat__entry--toggleable');
 
 [...toggables].map(elem => {
-  // elem.classList.toggle = '.JsonFormatter__entry--open';
+  // elem.classList.toggle = '.JFormat__entry--open';
   // console.log(elem.childNodes[1]);
 
   elem.childNodes[1].addEventListener('click', event => {
     console.log(elem);
-    elem.classList.toggle('JsonFormatter__entry--open');
+    elem.classList.toggle('JFormat__entry--open');
   });
 });
