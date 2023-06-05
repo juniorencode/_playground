48abb4533e432dc420a053831b5abb478a63d709
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-06-05 18:58:10 -0500

    feat(VirtualDOM): transform jsx

--- VirtualDOM/src/index.js
+++ VirtualDOM/src/index.js
@@ -1,1 +1,13 @@
-console.log('Hello World..!!');
+/** @jsx virtualDOM */
+
+const virtualDOM = (type, props, ...args) => {
+  const children = [...args];
+  return {
+    type,
+    props,
+    children
+  };
+};
+
+const title = <h1>Hello world..!!</h1>;
+console.log(title);
