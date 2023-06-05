953e307c4890ce1eceda218d834f8f272935622d
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-06-04 19:24:08 -0500

    chore(JsonFormatter): remove comments and console logs

--- JsonFormatter/main.js
+++ JsonFormatter/main.js
@@ -1,11 +1,7 @@
 const toggables = document.querySelectorAll('.JFormat__entry--toggleable');
 
 [...toggables].map(elem => {
-  // elem.classList.toggle = '.JFormat__entry--open';
-  // console.log(elem.childNodes[1]);
-
-  elem.childNodes[1].addEventListener('click', event => {
-    console.log(elem);
+  elem.childNodes[1].addEventListener('click', () => {
     elem.classList.toggle('JFormat__entry--open');
   });
 });
