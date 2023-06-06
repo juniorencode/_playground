38822a59eb076239e68bfc703c6ec1dbde52b18b
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-06-05 19:14:29 -0500

    feat(VirtualDOM): render with classname prop

--- VirtualDOM/src/index.js
+++ VirtualDOM/src/index.js
@@ -12,10 +12,17 @@ const virtualDOM = (type, props, ...args) => {
 const render = node => {
   const element = document.createElement(node.type);
   const children = document.createTextNode(node.children);
+
+  if (node.props) {
+    if (node.props.className) {
+      element.setAttribute('class', node.props.className);
+    }
+  }
+
   element.append(children);
   return element;
 };
 
-const title = <h1>Hello world..!!</h1>;
+const title = <h1 className="title">Hello world..!!</h1>;
 document.body.append(render(title));
 console.log(title);
