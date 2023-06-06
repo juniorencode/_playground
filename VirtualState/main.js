b905d4c49890af37eb9f3958e0e5831aacfd90a6
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-06-05 20:37:05 -0500

    feat(VirtualState): skeleton of functions

--- VirtualState/main.js
+++ VirtualState/main.js
@@ -1,1 +1,20 @@
-console.log('Hello World..!!');
+const createStore = (reducer, initialState) => {
+  const getState = () => {};
+
+  const dispatch = () => {};
+
+  const subscribe = () => {};
+
+  return {
+    getState,
+    dispatch,
+    subscribe
+  };
+};
+
+const reducer = () => {};
+
+const store = createStore(reducer, 'initial state');
+// store.getState
+// store.dispatch
+// store.subscribe
