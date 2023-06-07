6d0ed1b455aecec6d0b75c2466bd9da2f9d4ce4a
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-06-06 21:21:02 -0500

    feat(MarkdownNotebook): create note structure

--- MarkdownNotebook/main.js
+++ MarkdownNotebook/main.js
@@ -1,1 +1,15 @@
-console.log('Hello World..!!');
+const notebook = document.querySelector('.Notebook');
+
+const createNote = () => {
+  const box = document.createElement('div');
+  const input = document.createElement('div');
+
+  box.classList.add('Notebook__note');
+  input.classList.add('Notebook__input');
+  input.contentEditable = true;
+  box.append(input);
+
+  return box;
+};
+
+notebook.append(createNote());
