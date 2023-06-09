15045d4a3641731ebe0490c417f24368332d748d
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-06-09 17:33:31 -0500

    feat(AStarAlgorithm): draw scene

--- AStarAlgorithm/main.js
+++ AStarAlgorithm/main.js
@@ -8,8 +8,39 @@ let scene;
 
 // tiles
 const widthTile = parseInt(canvas.width / columns);
-const heigthTile = parseInt(canvas.heigth / rows);
+const heightTile = parseInt(canvas.height / rows);
+const bgFillTile = '#000';
+const bgEmptyTile = '#ccc';
 
+// objects
+class Box {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.type = 0;
+    this.color = bgEmptyTile;
+    this.f = 0; // total cost (g+h)
+    this.g = 0; // steps done
+    this.h = 0; // heuristics (estimate of what remains)
+    this.parent = null;
+    this.neighbors = [];
+
+    if (Math.floor(Math.random() * 5) === 1) this.type = 1;
+    if (this.type === 1) this.color = bgFillTile;
+  }
+
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(
+      this.x * widthTile,
+      this.y * heightTile,
+      widthTile,
+      heightTile
+    );
+  }
+}
+
+// helper functions
 const create2DArray = () => {
   const obj = new Array(rows);
 
@@ -20,14 +51,32 @@ const create2DArray = () => {
   return obj;
 };
 
-const loop = () => {
-  console.log('loop');
+const drawScene = () => {
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      // console.log(scene[i][j]);
+      scene[i][j].draw();
+    }
+  }
+};
 
+// looping
+const loop = () => {
+  drawScene();
   requestAnimationFrame(loop);
 };
 
 const init = () => {
   scene = create2DArray();
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+      scene[i][j] = new Box(j, i);
+    }
+  }
+
+  console.log(heightTile);
+
   loop();
 };
 
