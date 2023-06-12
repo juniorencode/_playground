bf367adc16879fb3c5c1b8fea43ea9fe8cd81cf8
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-06-12 15:52:38 -0500

    feat(AStarAlgorithm): rgba to hex

--- ColorLibrary/main.js
+++ ColorLibrary/main.js
@@ -13,8 +13,39 @@ class Color {
     const r = parseInt(rgbaValues[0].trim());
     const g = parseInt(rgbaValues[1].trim());
     const b = parseInt(rgbaValues[2].trim());
-    const a = parseInt(rgbaValues[3]?.trim()) || 1;
+    const a = parseFloat(rgbaValues[3]?.trim()) || 1;
 
     return { r, g, b, a };
+  }
+
+  rgbaToHex(rgba) {
+    return this.rgbToHex(this.mixAlpha(rgba));
+  }
+
+  mixAlpha(rgba) {
+    const { r, g, b, a } = rgba;
+    const mixedR = Math.round((1 - a) * 255 + a * r);
+    const mixedG = Math.round((1 - a) * 255 + a * g);
+    const mixedB = Math.round((1 - a) * 255 + a * b);
+
+    return { r: mixedR, g: mixedG, b: mixedB };
+  }
+
+  rgbToHex(rgb) {
+    const { r, g, b } = rgb;
+
+    return { hex: this.numbToHex(r) + this.numbToHex(g) + this.numbToHex(b) };
+  }
+
+  numbToHex(numb) {
+    return numb.toString(16).padStart(2, '0');
+  }
+
+  toHex() {
+    return this.rgbaToHex(this.rgba);
+  }
+
+  toHexString() {
+    return `#${this.toHex().hex}`;
   }
 }
