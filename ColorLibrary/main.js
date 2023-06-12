f18b7bd6420047ff61203e53c19d68aef935c5f5
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-06-12 13:50:35 -0500

    feat(AStarAlgorithm): parse rgba

--- ColorLibrary/main.js
+++ ColorLibrary/main.js
@@ -1,1 +1,20 @@
-console.log('Hello World..!!');
+class Color {
+  constructor(input) {
+    this.rgba = this.parseRGBA(input);
+
+    console.log(this.rgba);
+  }
+
+  parseRGBA(string) {
+    string = string.replace('rgba(', '').replace(')', '');
+
+    const rgbaValues = string.split(',');
+
+    const r = parseInt(rgbaValues[0].trim());
+    const g = parseInt(rgbaValues[1].trim());
+    const b = parseInt(rgbaValues[2].trim());
+    const a = parseInt(rgbaValues[3]?.trim()) || 1;
+
+    return { r, g, b, a };
+  }
+}
