8dd1897e400d3b8ddd1d7d8303a3ac82baefedc3
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-06-12 16:58:23 -0500

    feat(AStarAlgorithm): rgba to rgba

--- ColorLibrary/main.js
+++ ColorLibrary/main.js
@@ -1,8 +1,6 @@
 class Color {
   constructor(input) {
     this.rgba = this.parseRGBA(input);
-
-    console.log(this.rgba);
   }
 
   parseRGBA(string) {
@@ -22,6 +20,61 @@ class Color {
     return this.rgbToHex(this.mixAlpha(rgba));
   }
 
+  rgbToHex(rgb) {
+    const { r, g, b } = rgb;
+
+    return { hex: this.numbToHex(r) + this.numbToHex(g) + this.numbToHex(b) };
+  }
+
+  rgbaToCmyk(rgba) {
+    return this.rgbToCmyk(this.mixAlpha(rgba));
+  }
+
+  rgbToCmyk(rgb) {
+    const { r, g, b } = rgb;
+    const red = r / 255;
+    const green = g / 255;
+    const blue = b / 255;
+    let c = 1 - red;
+    let m = 1 - green;
+    let y = 1 - blue;
+    let k = Math.min(c, m, y);
+
+    c = Math.round((((c - k) / (1 - k)) * 100) / 100) || 0;
+    m = Math.round((((m - k) / (1 - k)) * 100) / 100) || 0;
+    y = Math.round((((y - k) / (1 - k)) * 100) / 100) || 0;
+    k = Math.round((k * 100) / 100) || 0;
+
+    return { c, m, y, k };
+  }
+
+  toRgba() {
+    return this.rgba;
+  }
+
+  toRgbaString() {
+    const color = this.toRgba();
+    return `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`;
+  }
+
+  toHex() {
+    return this.rgbaToHex(this.rgba);
+  }
+
+  toHexString() {
+    return `#${this.toHex().hex}`;
+  }
+
+  toCmyk() {
+    return this.rgbaToCmyk(this.rgba);
+  }
+
+  toCmykString() {
+    const color = this.toCmyk();
+    return `C${color.c} M${color.m} Y${color.y} K${color.k}`;
+  }
+
+  // helpers
   mixAlpha(rgba) {
     const { r, g, b, a } = rgba;
     const mixedR = Math.round((1 - a) * 255 + a * r);
@@ -31,21 +84,7 @@ class Color {
     return { r: mixedR, g: mixedG, b: mixedB };
   }
 
-  rgbToHex(rgb) {
-    const { r, g, b } = rgb;
-
-    return { hex: this.numbToHex(r) + this.numbToHex(g) + this.numbToHex(b) };
-  }
-
   numbToHex(numb) {
     return numb.toString(16).padStart(2, '0');
-  }
-
-  toHex() {
-    return this.rgbaToHex(this.rgba);
-  }
-
-  toHexString() {
-    return `#${this.toHex().hex}`;
   }
 }
