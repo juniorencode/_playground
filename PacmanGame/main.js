b11183a5ecbee7c6b67055c006409d3e2d7055d8
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-06-16 16:26:03 -0500

    feat(PacmanGame): draw walls

--- PacmanGame/main.js
+++ PacmanGame/main.js
@@ -28,3 +28,90 @@ const map = [
   [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ];
+
+const oneBlockSize = 18;
+const wallSpaceWidth = oneBlockSize / 1.2;
+const wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
+const wallInnerColor = 'black';
+
+const createRect = (x, y, width, height, color) => {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height);
+};
+
+const drawWalls = () => {
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[0].length; j++) {
+      if (map[i][j] == 1) {
+        createRect(
+          j * oneBlockSize,
+          i * oneBlockSize,
+          oneBlockSize,
+          oneBlockSize,
+          '#342DCA'
+        );
+
+        if (j > 0 && map[i][j - 1] == 1) {
+          createRect(
+            j * oneBlockSize,
+            i * oneBlockSize + wallOffset,
+            wallSpaceWidth + wallOffset,
+            wallSpaceWidth,
+            wallInnerColor
+          );
+        }
+
+        if (j < map[0].length - 1 && map[i][j + 1] == 1) {
+          createRect(
+            j * oneBlockSize + wallOffset,
+            i * oneBlockSize + wallOffset,
+            wallSpaceWidth + wallOffset,
+            wallSpaceWidth,
+            wallInnerColor
+          );
+        }
+
+        if (i < map.length - 1 && map[i + 1][j] == 1) {
+          createRect(
+            j * oneBlockSize + wallOffset,
+            i * oneBlockSize + wallOffset,
+            wallSpaceWidth,
+            wallSpaceWidth + wallOffset,
+            wallInnerColor
+          );
+        }
+
+        if (i > 0 && map[i - 1][j] == 1) {
+          createRect(
+            j * oneBlockSize + wallOffset,
+            i * oneBlockSize,
+            wallSpaceWidth,
+            wallSpaceWidth + wallOffset,
+            wallInnerColor
+          );
+        }
+      }
+    }
+  }
+};
+
+const update = () => {};
+
+const draw = () => {
+  drawWalls();
+};
+
+const loop = () => {
+  update();
+  draw();
+  requestAnimationFrame(loop);
+};
+
+const init = () => {
+  canvas.width = map[0].length * oneBlockSize;
+  canvas.height = map.length * oneBlockSize;
+
+  loop();
+};
+
+init();
