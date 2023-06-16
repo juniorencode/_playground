3fc152d8ca2678395c668238340ec216ac23780e
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-06-15 21:24:20 -0500

    feat(TypingGameText): variables declaration

--- TypingGameText/main.js
+++ TypingGameText/main.js
@@ -1,1 +1,12 @@
-console.log('Hello World..!!');
+const typingText = document.querySelector('.typing p');
+const inpField = document.querySelector('.input');
+const tryAgainBtn = document.querySelector('.button button');
+const timeTag = document.querySelector('.time span');
+const mistakeTag = document.querySelector('.mistake span');
+const wpmTag = document.querySelector('.wpm span');
+const cpmTag = document.querySelector('.cpm span');
+
+let timer;
+let maxTime = 60;
+let timeLeft = maxTime;
+let charIndex = (mistakes = isTyping = 0);
