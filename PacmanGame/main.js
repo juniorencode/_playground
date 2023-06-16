2bd1b791445205e72f1171973c28b7e6c3d6e28c
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-06-16 16:54:44 -0500

    feat(PacmanGame): draw pacman

--- PacmanGame/main.js
+++ PacmanGame/main.js
@@ -1,7 +1,12 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-const pacmanFrames = document.getElementById('animation');
+const pacmanFrames = document.getElementById('animations');
 const ghostFrames = document.getElementById('ghosts');
+
+const DIRECTION_UP = 1;
+const DIRECTION_RIGHT = 2;
+const DIRECTION_BOTTOM = 3;
+const DIRECTION_LEFT = 4;
 
 const map = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -33,6 +38,49 @@ const oneBlockSize = 18;
 const wallSpaceWidth = oneBlockSize / 1.2;
 const wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
 const wallInnerColor = 'black';
+
+let pacman;
+
+class Pacman {
+  constructor(x, y, width, height, speed) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.speed = speed;
+    this.direction = DIRECTION_RIGHT;
+    this.currentFrame = 1;
+  }
+
+  draw() {
+    ctx.save();
+    ctx.translate(this.x + oneBlockSize / 2, this.y + oneBlockSize / 2);
+    ctx.rotate((this.direction * 90 * Math.PI) / 180);
+    ctx.translate(-this.x - oneBlockSize / 2, -this.y - oneBlockSize / 2);
+    ctx.drawImage(
+      pacmanFrames,
+      (this.currentFrame - 1) * oneBlockSize,
+      0,
+      oneBlockSize,
+      oneBlockSize,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+    ctx.restore();
+  }
+}
+
+let createNewPacman = () => {
+  pacman = new Pacman(
+    oneBlockSize,
+    oneBlockSize,
+    oneBlockSize,
+    oneBlockSize,
+    oneBlockSize / 5
+  );
+};
 
 const createRect = (x, y, width, height, color) => {
   ctx.fillStyle = color;
@@ -99,6 +147,7 @@ const update = () => {};
 
 const draw = () => {
   drawWalls();
+  pacman.draw();
 };
 
 const loop = () => {
@@ -111,6 +160,7 @@ const init = () => {
   canvas.width = map[0].length * oneBlockSize;
   canvas.height = map.length * oneBlockSize;
 
+  createNewPacman();
   loop();
 };
 
