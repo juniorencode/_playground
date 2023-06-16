22cf369a61400b54468251be854153583bd5315f
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-06-15 21:27:35 -0500

    feat(TypingGameText): load paragraph

--- TypingGameText/main.js
+++ TypingGameText/main.js
@@ -10,3 +10,18 @@ let timer;
 let maxTime = 60;
 let timeLeft = maxTime;
 let charIndex = (mistakes = isTyping = 0);
+
+loadParagraph = () => {
+  const ranIndex = Math.floor(Math.random() * paragraphs.length);
+  typingText.innerHTML = '';
+
+  paragraphs[ranIndex].split('').forEach(char => {
+    let span = `<span>${char}</span>`;
+    typingText.innerHTML += span;
+  });
+
+  document.addEventListener('keydown', () => inpField.focus());
+  typingText.addEventListener('click', () => inpField.focus());
+};
+
+loadParagraph();
