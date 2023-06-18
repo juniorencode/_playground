58a316632d9a3663ca30fd85c90295846d755e2a
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-06-18 09:26:44 -0500

    feat(InputImageCut): handle click input

--- InputImageCut/main.js
+++ InputImageCut/main.js
@@ -1,1 +1,29 @@
-console.log('Hello World..!!');
+class InputImageCut {
+  constructor(container) {
+    this.container = container;
+    this.input = document.createElement('input');
+    this.input.type = 'file';
+
+    this.drawbanner();
+    this.container.addEventListener('click', () => this.handleInput());
+  }
+
+  handleInput() {
+    this.input.click();
+  }
+
+  drawbanner() {
+    const banner = document.createElement('div');
+    const icon = document.createElement('i');
+    const paragraph = document.createElement('p');
+    const text = document.createTextNode(
+      'Drop your photo here or browse your computer'
+    );
+    banner.classList.add('InputImageCut__banner');
+    icon.classList.add('far', 'fa-image');
+    paragraph.append(text);
+    banner.append(icon);
+    banner.append(paragraph);
+    this.container.append(banner);
+  }
+}
