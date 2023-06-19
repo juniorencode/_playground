2a625afd0d54a29af42831d5c6ca57ebef348736
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-06-18 21:30:11 -0500

    feat(InputImageCut): remove events default

--- InputImageCut/main.js
+++ InputImageCut/main.js
@@ -4,69 +4,35 @@ class InputImageCut {
       throw new Error('InputImageCut constructor: missing arguments');
 
     this.container = options.container;
-    this.file = document.createElement('input');
-    this.file.type = 'file';
-    this.imageTexture = new Image();
 
-    // canvas
-    this.background = null;
-    this.filter = null;
-
-    this._eventHandlers = [];
+    // events manager
+    this._eventHandlers = {};
 
     this.init();
   }
 
   init() {
-    this.appendBanner();
+    this.file = document.createElement('input');
+    this.file.type = 'file';
+    this.imageTexture = new Image();
+
+    // canvas
     this.background = this.buildCanvas('background');
     this.filter = this.buildCanvas('filter');
-  }
 
-  buildCanvas(name) {
-    const object = {};
-    object.canvas = document.createElement('canvas');
-    object.canvas.classList.add('InputImageCut__' + name);
-    object.ctx = object.canvas.getContext('2d');
-    return object;
-  }
-
-  handleOpenInputFile() {
-    this.file.click();
-  }
-
-  handleSelectFile() {
-    this.imageTexture.src = URL.createObjectURL(this.file.files[0]);
-  }
-
-  handleUploadFile() {
-    this.container.innerHTML = '';
-    this.appendBackgroundCanvas();
-    this.appendFilterCanvas();
-    this.drawBackground();
+    // start
+    this.appendBanner();
+    this.addEventsDefault();
   }
 
   drawBackground() {
-    const ctx = this.backgroundCanvas.getContext('2d');
-    ctx.drawImage(
+    this.background.ctx.drawImage(
       this.imageTexture,
       0,
       0,
       this.imageTexture.width,
       this.imageTexture.height
     );
-  }
-
-  appendBackgroundCanvas() {
-    this.backgroundCanvas = document.createElement('canvas');
-    this.backgroundCanvas.classList.add('InputImageCut__background');
-    this.container.append(this.backgroundCanvas);
-  }
-
-  appendFilterCanvas() {
-    this.filterCanvas = document.createElement('canvas');
-    this.filterCanvas.classList.add('InputImageCut__filter');
-    this.container.append(this.filterCanvas);
   }
 
   appendBanner() {
@@ -84,7 +50,47 @@ class InputImageCut {
     this.container.append(banner);
   }
 
-  // event manager
+  appendCanvas() {
+    this.clearContainer();
+    this.container.append(this.background.canvas);
+    this.container.append(this.background.filter);
+  }
+
+  clearContainer() {
+    this.container.innerHTML = '';
+  }
+
+  // handlers event
+  handleOpenInputFile() {
+    this.file.click();
+  }
+
+  handleSelectFile() {
+    const file = this.file.files[0];
+    if (!file) return;
+    this.imageTexture.src = URL.createObjectURL(file);
+  }
+
+  handleUploadFile() {
+    this.removeEventsDefault();
+    this.appendCanvas();
+    this.drawBackground();
+  }
+
+  // events pack
+  addEventsDefault() {
+    this.addListener(this.container, 'click', () => this.handleOpenInputFile());
+    this.addListener(this.file, 'change', () => this.handleSelectFile());
+    this.addListener(this.imageTexture, 'load', () => this.handleUploadFile());
+  }
+
+  removeEventsDefault() {
+    this.removeListeners(this.container, 'click');
+    this.removeListeners(this.file, 'change');
+    this.removeListeners(this.imageTexture, 'load');
+  }
+
+  // events manager
   addListener(node, event, handler, capture = false) {
     if (!(node in this._eventHandlers)) this._eventHandlers[node] = {};
     if (!(event in this._eventHandlers[node]))
@@ -114,5 +120,14 @@ class InputImageCut {
     for (const event in handlers) {
       this.removeListenersBase(node, event, handlers);
     }
+  }
+
+  // helpers
+  buildCanvas(name) {
+    const object = {};
+    object.canvas = document.createElement('canvas');
+    object.canvas.classList.add('InputImageCut__' + name);
+    object.ctx = object.canvas.getContext('2d');
+    return object;
   }
 }
