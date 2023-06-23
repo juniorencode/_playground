083b39494a2c541215067cdd5fba5bd3a5b69143
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-06-22 20:13:59 -0500

    feat(PixelDraw): change pincel size

--- PixelDraw/main.js
+++ PixelDraw/main.js
@@ -1,12 +1,14 @@
 class PixelDraw {
   constructor(container) {
     this.container = container;
-    this.appendTools();
-    this.appendCanvas();
 
     this.pixelSize = 10;
+    this.gridUnitSize = 10;
     this.color = '#000';
     this.isPress = false;
+
+    this.appendTools();
+    this.appendCanvas();
 
     this.canvas.addEventListener('mousedown', e => this.handleMouseDown(e));
     this.canvas.addEventListener('mouseup', e => this.handleMouseUp(e));
@@ -29,6 +31,7 @@ class PixelDraw {
 
   handleMouseMove(e) {
     e.preventDefault();
+    // this.motionPencil(e);
     this.motion(e);
   }
 
@@ -41,30 +44,62 @@ class PixelDraw {
     this.color = e.target.value;
   }
 
+  handleSetSize(e) {
+    this.pixelSize = e.target.value * this.gridUnitSize;
+  }
+
+  motionPencil(e) {
+    this.clearCanvas();
+    const x = Math.floor(
+      (e.clientX - this.canvas.offsetLeft) / this.gridUnitSize
+    );
+    const y = Math.floor(
+      (e.clientY - this.canvas.offsetTop) / this.gridUnitSize
+    );
+
+    this.drawPencil(x, y);
+  }
+
   motion(e) {
     if (!this.isPress) return;
+    const x = Math.floor(
+      (e.clientX - this.canvas.offsetLeft) / this.gridUnitSize
+    );
+    const y = Math.floor(
+      (e.clientY - this.canvas.offsetTop) / this.gridUnitSize
+    );
 
-    const x = Math.floor((e.clientX - this.canvas.offsetLeft) / this.pixelSize);
-    const y = Math.floor((e.clientY - this.canvas.offsetTop) / this.pixelSize);
     this.drawPixel(x, y);
   }
 
-  drawPixel(x, y) {
-    const size = this.pixelSize;
-    this.ctx.fillStyle = this.color;
-    this.ctx.fillRect(x * size, y * size, size, size);
+  clearCanvas() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.drawGrid();
   }
 
   appendTools() {
     const div = document.createElement('div');
+    const width = document.createElement('div');
+    const labelWidth = document.createElement('label');
+    const inputWidth = document.createElement('input');
     const color = document.createElement('div');
     const labelColor = document.createElement('label');
     const inputColor = document.createElement('input');
+    div.classList.add('PixelDraw__tools');
+    width.classList.add('PixelDraw__width');
+    labelWidth.innerText = 'Line: ';
+    inputWidth.type = 'number';
+    inputWidth.value = this.pixelSize / this.gridUnitSize;
+    inputWidth.addEventListener('input', e => this.handleSetSize(e));
+    color.classList.add('PixelDraw__color');
     labelColor.innerText = 'Color: ';
     inputColor.type = 'color';
     inputColor.addEventListener('input', e => this.handleSetColor(e));
+    width.append(labelWidth);
+    width.append(inputWidth);
     color.append(labelColor);
     color.append(inputColor);
+    div.append(width);
     div.append(color);
     this.container.append(div);
   }
@@ -79,7 +114,7 @@ class PixelDraw {
   }
 
   drawGrid() {
-    const size = this.pixelSize;
+    const size = this.gridUnitSize;
 
     for (let y = 0; y < this.canvas.height; y += size) {
       for (let x = 0; x < this.canvas.width; x += size) {
@@ -90,5 +125,22 @@ class PixelDraw {
         this.ctx.fillRect(x, y, size, size);
       }
     }
+  }
+
+  drawPixel(x, y) {
+    const size = this.pixelSize;
+    this.ctx.fillStyle = this.color;
+    this.ctx.fillRect(x * this.gridUnitSize, y * this.gridUnitSize, size, size);
+  }
+
+  drawPencil(x, y) {
+    const size = this.pixelSize;
+    this.ctx.strokeStyle = '#000';
+    this.ctx.strokeRect(
+      x * this.gridUnitSize,
+      y * this.gridUnitSize,
+      size,
+      size
+    );
   }
 }
