bfb2b492f898a6f39f9146c8d4e9e4f1473ab5ec
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-06-24 18:19:05 -0500

    feat(RichTextEditor): highlight function

--- RichTextEditor/main.js
+++ RichTextEditor/main.js
@@ -8,6 +8,7 @@ const alignButtons = document.querySelectorAll('.align');
 const spacingButtons = document.querySelectorAll('.format');
 const scriptButtons = document.querySelectorAll('.script');
 
+// List of fonts
 const fontList = [
   'Arial',
   'Verdana',
@@ -17,3 +18,38 @@ const fontList = [
   'Courier New',
   'Cursive'
 ];
+
+// Initial settings
+const initializer = () => {
+  // function calls for highlighting buttons
+  // no highlights for link, unlink, lists, undo, redo since they are no time operations
+  highlighter(alignButtons, true);
+  highlighter(spacingButtons, true);
+  highlighter(formatButtons, false);
+  highlighter(scriptButtons, true);
+};
+
+// Highlight clicked button
+const highlighter = (className, needsRemoval) => {
+  className.forEach(button => {
+    button.addEventListener('click', () => {
+      // needsRemoval = true means only one button should be highlight and other would be normal
+      if (needsRemoval) {
+        let alreadyActive = false;
+
+        // if currently clicked button is already active
+        if (button.classList.contains('active')) {
+          alreadyActive = true;
+        }
+
+        // remove highlight from other buttons
+        highlighterRemover(className);
+
+        // highlight clicked button
+        if (!alreadyActive) button.classList.add('active');
+      } else {
+        // if other buttons can be highlighted
+      }
+    });
+  });
+};
