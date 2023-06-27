bb71c8df286e57729a2a8701577b5c19bb651736
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-06-27 07:54:07 -0500

    feat(FinanceDashboardApp): navigation

--- FinanceDashboardApp/client/src/App.tsx
+++ FinanceDashboardApp/client/src/App.tsx
@@ -1,5 +1,31 @@
+import { useMemo } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box, CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { Navbar } from '@/scenes/navbar';
+import { themeSettings } from './theme';
+
 function App() {
-  return <div className="App"></div>;
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>dashboard page</div>} />
+              <Route
+                path="/predictions"
+                element={<div>predictions page</div>}
+              />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
