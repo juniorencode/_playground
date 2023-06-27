083c446e5a713898132c646fb0f8b56bc4ccb4aa
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-06-27 08:59:39 -0500

    feat(FinanceDashboardApp): dashboard layout

--- FinanceDashboardApp/client/src/App.tsx
+++ FinanceDashboardApp/client/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box, CssBaseline, ThemeProvider } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
 import { Navbar } from '@/scenes/navbar';
+import { Dashboard } from '@/scenes/dashboard';
 import { themeSettings } from './theme';
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
             <Navbar />
             <Routes>
-              <Route path="/" element={<div>dashboard page</div>} />
+              <Route path="/" element={<Dashboard />} />
               <Route
                 path="/predictions"
                 element={<div>predictions page</div>}
