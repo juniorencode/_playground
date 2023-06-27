657ec04f3f908289f482bfe07883eda0ce6a98dd
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-06-27 11:52:09 -0500

    feat(FinanceDashboardApp): mongodb setup

--- FinanceDashboardApp/server/index.js
+++ FinanceDashboardApp/server/index.js
@@ -18,3 +18,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 console.log('Hello World..!!');
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(async () => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch(error => console.log(`${error} did not connect`));
