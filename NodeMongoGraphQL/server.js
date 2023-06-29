c03a8b6879d0da7b9ed2e1a1aa93fe1aa5dd9f6f
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-06-29 00:38:03 -0500

    feat(NodeMongoGraphQL): graphql setup

---
+++ NodeMongoGraphQL/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./graphql/schema');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my grahql api');
+});
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+app.listen(3000);
+console.log('Server is running on port 3000');
