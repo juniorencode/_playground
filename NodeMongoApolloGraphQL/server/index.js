271aa3dc39285aa6df6d1a97e9180230be1e16ba
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-07-01 17:59:11 -0500

    feat(NodeMongoApolloGraphQL): graphql setup

--- NodeMongoApolloGraphQL/server/index.js
+++ NodeMongoApolloGraphQL/server/index.js
@@ -1,6 +1,5 @@
-import express from 'express';
+import { startApolloServer } from './app.js';
+import { typeDefs } from './graphql/typeDefs.js';
+import { resolvers } from './graphql/resolvers.js';
 
-const app = express();
-
-app.listen(3000);
-console.log('Server on port 3000');
+startApolloServer(typeDefs, resolvers);
