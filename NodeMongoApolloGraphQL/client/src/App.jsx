a05cf0effc83d3ded6044f1a65bbee8c3d900eb7
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-07-01 22:10:13 -0500

    feat(NodeMongoApolloGraphQL): setup apollo graphql in client

--- NodeMongoApolloGraphQL/client/src/App.jsx
+++ NodeMongoApolloGraphQL/client/src/App.jsx
@@ -1,5 +1,16 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
+
 const App = () => {
-  return <div>Hello World..!!</div>;
+  return (
+    <ApolloProvider client={client}>
+      <h1>Hello World..!!</h1>
+    </ApolloProvider>
+  );
 };
 
 export default App;
