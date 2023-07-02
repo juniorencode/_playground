fbdcddd3124d6bb7abab695f65a89ac739c0c038
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-07-01 19:47:59 -0500

    feat(NodeMongoApolloGraphQL): mutation create project

--- NodeMongoApolloGraphQL/server/graphql/resolvers.js
+++ NodeMongoApolloGraphQL/server/graphql/resolvers.js
@@ -1,5 +1,19 @@
+import Project from '../models/Project.js';
+
 export const resolvers = {
   Query: {
     hello: () => 'Hello World..!!'
+  },
+  Mutation: {
+    createProject: async (_, args) => {
+      const project = new Project({
+        name: args.name,
+        description: args.description
+      });
+
+      const savedProject = await project.save();
+
+      return savedProject;
+    }
   }
 };
