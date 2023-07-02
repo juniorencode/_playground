850a861992f96a208a475a4b30f458e6a5a99538
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-07-01 23:37:48 -0500

    feat(NodeMongoApolloGraphQL/client): delete task

--- NodeMongoApolloGraphQL/client/src/components/tasks/TaskCard.jsx
+++ NodeMongoApolloGraphQL/client/src/components/tasks/TaskCard.jsx
@@ -1,7 +1,27 @@
+import { useMutation } from '@apollo/client';
+import { DELETE_TASK } from '../../graphql/tasks';
+
 const TaskCard = ({ task }) => {
+  const [deleteTask] = useMutation(DELETE_TASK, {
+    refetchQueries: ['getProject']
+  });
+
   return (
     <div>
       <h2>{task.title}</h2>
+      <div>
+        <button
+          onClick={() => {
+            deleteTask({
+              variables: {
+                id: task._id
+              }
+            });
+          }}
+        >
+          DELETE
+        </button>
+      </div>
     </div>
   );
 };
