6ee2e94131a18dd2b2fefb0e1169cee861c4a70b
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-07-02 10:57:30 -0500

    feat(NpmLinkApp): initial

---
+++ NpmLinkApp/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from 'npmlinklibrary';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello World..!!</h1>
+      <Button />
+    </>
+  );
+};
+
+export default App;
