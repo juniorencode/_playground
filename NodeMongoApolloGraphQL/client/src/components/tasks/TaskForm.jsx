6630e524687afc41f515bcd1562e4ad22226c785
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-07-01 23:36:39 -0500

    feat(NodeMongoApolloGraphQL/client): create task

--- NodeMongoApolloGraphQL/client/src/components/tasks/TaskForm.jsx
+++ NodeMongoApolloGraphQL/client/src/components/tasks/TaskForm.jsx
@@ -1,5 +1,32 @@
+import { useMutation } from '@apollo/client';
+import { useParams } from 'react-router-dom';
+import { CREATE_TASK } from '../../graphql/tasks';
+
 const TaskForm = () => {
-  return <p>Task Form</p>;
+  const params = useParams();
+  const [createTask] = useMutation(CREATE_TASK, {
+    refetchQueries: ['getProject']
+  });
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const result = await createTask({
+      variables: {
+        title: e.target.title.value,
+        projectId: params.id
+      }
+    });
+    console.log(result);
+    e.target.reset();
+    e.target.title.focus();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="title" placeholder="Write a task" />
+      <button>Add</button>
+    </form>
+  );
 };
 
 export { TaskForm };
