4d1406595e3f9e9dd1579beeafd3b3c5b7d7187d
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-07-01 22:42:38 -0500

    feat(NodeMongoApolloGraphQL): setup frontend

--- NodeMongoApolloGraphQL/client/src/App.jsx
+++ NodeMongoApolloGraphQL/client/src/App.jsx
@@ -1,4 +1,9 @@
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+// pages
+import ProjectDetails from './pages/ProjectDetails';
+import Projects from './pages/Projects';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
@@ -8,7 +13,15 @@ const client = new ApolloClient({
 const App = () => {
   return (
     <ApolloProvider client={client}>
-      <h1>Hello World..!!</h1>
+      <BrowserRouter>
+        <div className="container m-auto h-screen flex items-center justify-center">
+          <Routes>
+            <Route path="/" element={<Navigate to="/projects" replace />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/projects/:id" element={<ProjectDetails />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </ApolloProvider>
   );
 };
