f420bf4d76c0836e67ff37b0765df9984e052384
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-07-02 22:11:41 -0500

    feat(UploadFileNodeMulter): recive files

--- UploadFileNodeMulter/server.js
+++ UploadFileNodeMulter/server.js
@@ -1,1 +1,22 @@
-console.log('Hello World..!!');
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
+
+const app = express();
+app.use(cors()); // allows incoming request from any IP
+
+const uploads = multer({ dest: __dirname + '/uploads' });
+
+app.get('/', (req, res) => {
+  res.json({ username: 'juniorencode' });
+});
+
+app.post('/uploads', uploads.array('files'), (req, res) => {
+  console.log(req.body);
+  console.log(req.files);
+  res.json({ status: 'files received' });
+});
+
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
+});
