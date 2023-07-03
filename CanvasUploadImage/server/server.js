2d9bd1bb714b59bab488cb9a1485868e811529e5
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-07-02 23:48:54 -0500

    feat(CanvasUploadImage): create server

---
+++ CanvasUploadImage/server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
+
+const app = express();
+app.use(cors()); // allows incoming request from any IP
+
+const storage = multer.diskStorage({
+  destination: (req, res, callback) => callback(null, __dirname + '/uploads'),
+  filename: (req, file, callback) => callback(null, file.originalname)
+});
+
+const uploads = multer({ storage });
+
+app.get('/', (req, res) => {
+  res.json({ username: 'juniorencode' });
+});
+
+app.post('/uploads', uploads.array('files'), (req, res) => {
+  console.log(req.body);
+  console.log(req.files);
+  res.json({ status: 'files received' });
+});
+
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
+});
