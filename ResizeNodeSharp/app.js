9dc3e97d76e0d4cd71179feaa6cf7132a26b07e8
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-07-02 23:02:28 -0500

    feat(ResizeNodeSharp): many sharp variants

--- ResizeNodeSharp/app.js
+++ ResizeNodeSharp/app.js
@@ -1,1 +1,20 @@
-console.log('Hello World..!!');
+const sharp = require('sharp');
+
+sharp('./input.jpg').blur(25).toFile('output0.jpg');
+sharp('./input.jpg').grayscale().toFile('output1.jpg');
+sharp('./input.jpg').rotate(90).toFile('output2.jpg');
+sharp('./input.jpg').flip().toFile('output3.jpg');
+sharp('./input.jpg').negate().toFile('output4.jpg');
+sharp('./input.jpg')
+  .modulate({ brightness: 1.2, saturation: 0.8, contrast: 1.1 })
+  .toFile('output5.jpg');
+sharp('./input.jpg').resize(200, 200).toFile('output6.jpg');
+sharp('./input.jpg')
+  .resize(200, 200, {
+    fit: 'contain',
+    background: { r: 0, b: 0, g: 0, alpha: 0 }
+  })
+  .toFile('output7.jpg');
+sharp('./input.jpg')
+  .extract({ left: 680, top: 200, width: 200, height: 330 })
+  .toFile('output8.jpg');
