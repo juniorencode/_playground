39b02ec8175c7605919094540d3fa9f375b2fb9f
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-07-02 23:40:33 -0500

    feat(CanvasUploadImage): draw image in canvas

--- CanvasUploadImage/client/main.js
+++ CanvasUploadImage/client/main.js
@@ -1,1 +1,14 @@
-console.log('Hello World..!!');
+const image = document.querySelector('img');
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+image.addEventListener('load', () => {
+  console.log('load..!!');
+
+  canvas.width = image.width;
+  canvas.height = image.height;
+
+  ctx.drawImage(image, 0, 0);
+  ctx.fillStyle = 'green';
+  ctx.fillRect(150, 150, 100, 100);
+});
