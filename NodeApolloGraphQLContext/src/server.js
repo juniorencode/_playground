253916768e541c165730de78d2ca9613c4832bbd
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-07-03 21:13:27 -0500

    feat(NodeApolloGraphQLContext): types and resolvers

--- NodeApolloGraphQLContext/src/server.js
+++ NodeApolloGraphQLContext/src/server.js
@@ -1,1 +1,7 @@
-console.log('Hello World..!!');
+import connectDB from './db.js';
+import startApolloServer from './app.js';
+import typeDefs from './graphql/typeDefs.js';
+import resolvers from './graphql/resolvers.js';
+
+connectDB();
+startApolloServer(typeDefs, resolvers);
