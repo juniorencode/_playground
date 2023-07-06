1cb3e5550195b995ed89d66d68e34c2a6c62910c
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-07-05 23:04:29 -0500

    refactor(SkeletonLoadingAnimation): set time out for fetch

--- SkeletonLoadingAnimation/main.js
+++ SkeletonLoadingAnimation/main.js
@@ -1,20 +1,26 @@
 const grid = document.querySelector('.grid');
 const cardTemplate = document.getElementById('card-template');
 
-fetch('https://jsonplaceholder.typicode.com/posts')
-  .then(res => res.json())
-  .then(posts => {
-    grid.innerHTML = '';
-    posts.forEach(post => {
-      const div = cardTemplate.content.cloneNode(true);
+for (let i = 0; i < 100; i++) {
+  grid.append(cardTemplate.content.cloneNode(true));
+}
 
-      const image = new Image();
-      image.src = 'https://source.unsplash.com/100x100/?nature';
+setTimeout(() => {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(posts => {
+      grid.innerHTML = '';
+      posts.forEach(post => {
+        const div = cardTemplate.content.cloneNode(true);
 
-      div.querySelector('[data-image]').append(image);
-      div.querySelector('[data-title]').textContent = post.title;
-      div.querySelector('[data-body]').textContent = post.body;
+        const image = new Image();
+        image.src = 'https://source.unsplash.com/100x100/?nature';
 
-      grid.append(div);
+        div.querySelector('[data-image]').append(image);
+        div.querySelector('[data-title]').textContent = post.title;
+        div.querySelector('[data-body]').textContent = post.body;
+
+        grid.append(div);
+      });
     });
-  });
+}, 3000);
