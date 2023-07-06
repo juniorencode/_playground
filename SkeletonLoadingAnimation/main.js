34a51209f37a08230ea1512b1831a6febe2343c1
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-07-05 22:59:19 -0500

    fix(SkeletonLoadingAnimation): image load

--- SkeletonLoadingAnimation/main.js
+++ SkeletonLoadingAnimation/main.js
@@ -1,20 +1,20 @@
 const grid = document.querySelector('.grid');
 const cardTemplate = document.getElementById('card-template');
 
-for (let i = 0; i < 100; i++) {
-  grid.append(cardTemplate.content.cloneNode(true));
-}
-
 fetch('https://jsonplaceholder.typicode.com/posts')
   .then(res => res.json())
   .then(posts => {
     grid.innerHTML = '';
     posts.forEach(post => {
       const div = cardTemplate.content.cloneNode(true);
-      div.querySelector('[data-image]').src =
-        'https://source.unsplash.com/100x100/?nature';
+
+      const image = new Image();
+      image.src = 'https://source.unsplash.com/100x100/?nature';
+
+      div.querySelector('[data-image]').append(image);
       div.querySelector('[data-title]').textContent = post.title;
       div.querySelector('[data-body]').textContent = post.body;
+
       grid.append(div);
     });
   });
