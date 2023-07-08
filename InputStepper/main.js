60d5031cd17284e741f75fc4011195ad029cc79d
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-07-07 21:42:41 -0500

    feat(InputStepper): decrement and increment

--- InputStepper/main.js
+++ InputStepper/main.js
@@ -1,1 +1,21 @@
-console.log('Hello World..!!');
+const input = document.querySelector('input');
+const min = parseInt(input.getAttribute('min'));
+const max = parseInt(input.getAttribute('max'));
+const step = parseInt(input.getAttribute('step'));
+const btnDecrement = document.getElementById('decrement');
+const btnIncrement = document.getElementById('increment');
+
+const decrement = () => {
+  const value = parseInt(input.value) - 1;
+  if (value < min || value > max) return;
+  input.value = value;
+};
+
+const increment = () => {
+  const value = parseInt(input.value) + 1;
+  if (value < min || value > max) return;
+  input.value = value;
+};
+
+btnDecrement.addEventListener('click', decrement);
+btnIncrement.addEventListener('click', increment);
