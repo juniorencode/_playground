4785332486dfa99c97341974dfa2f8a523a716e2
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-07-08 21:41:03 -0500

    feat(InputMultipleUploadFile): not choosen any file

--- InputMultipleUploadFile/main.js
+++ InputMultipleUploadFile/main.js
@@ -5,4 +5,10 @@ document.querySelector('.file-input').addEventListener('change', () => {
   let allowed_size_mb = 100;
 
   const files_input = document.querySelector('.file-input').files;
+
+  // user has not choosen any file
+  if (files_input.length === 0) {
+    alert('Error: No file selected');
+    return;
+  }
 });
