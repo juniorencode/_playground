e955924b48edd277729affd9d9cc1811797e714a
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-07-08 21:40:14 -0500

    feat(InputMultipleUploadFile): variables initial

--- InputMultipleUploadFile/main.js
+++ InputMultipleUploadFile/main.js
@@ -1,1 +1,8 @@
-console.log('Hello World..!!');
+document.querySelector('.file-input').addEventListener('change', () => {
+  let allowed_mine_types = [];
+
+  // allowed max size in MB
+  let allowed_size_mb = 100;
+
+  const files_input = document.querySelector('.file-input').files;
+});
