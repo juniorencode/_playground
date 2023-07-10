c2eaa966bbee3a43240e3cab7e0a131b449a5c84
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-07-09 19:53:58 -0500

    feat(InputPredectiveText): suggestion words

--- InputPredectiveText/main.js
+++ InputPredectiveText/main.js
@@ -1,1 +1,28 @@
-console.log('Hello World..!!');
+const words = [
+  'Apple',
+  'Pencil',
+  'Pen',
+  'Chair',
+  'Helmet',
+  'Grapes',
+  'Tub',
+  'Trophy',
+  'Cookie',
+  'Donut',
+  'Shirt',
+  'Bat',
+  'Ash',
+  'Bell',
+  'Chat',
+  'Ball',
+  'Eye',
+  'Fish',
+  'Zip',
+  'Game',
+  'Juice',
+  'Orange',
+  'Fan',
+  'Ice'
+];
+
+words.sort();
