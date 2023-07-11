6cc44c040174a0b5bb7768ab1da62a1451e5d3e7
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-07-10 21:42:43 -0500

    style(UIComponents): input password with show and style

--- UIComponents/src/components/InputPassword/index.jsx
+++ UIComponents/src/components/InputPassword/index.jsx
@@ -2,11 +2,12 @@ import './InputPassword.css';
 
 const InputPassword = () => {
   return (
-    <input
-      className="InputPassword"
-      placeholder="Type something..."
-      type="password"
-    />
+    <div className="InputPassword">
+      <input placeholder="Type password..." type="password" />
+      <button>
+        <span className="icon-eye-blocked"></span>
+      </button>
+    </div>
   );
 };
 
