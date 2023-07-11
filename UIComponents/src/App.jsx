16a95f9a2adf9374d0a9959db51a83c9326cfc73
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-07-10 21:21:38 -0500

    feat(UIComponents): input password

--- UIComponents/src/App.jsx
+++ UIComponents/src/App.jsx
@@ -1,10 +1,12 @@
 import { InputText } from './components/InputText';
+import { InputPassword } from './components/InputPassword';
 import './App.css';
 
 function App() {
   return (
     <div className="container">
       <InputText />
+      <InputPassword />
     </div>
   );
 }
---
+++ UIComponents/src/components/InputPassword/index.jsx
@@ -0,0 +1,13 @@
+import './InputPassword.css';
+
+const InputPassword = () => {
+  return (
+    <input
+      className="InputPassword"
+      placeholder="Type something..."
+      type="password"
+    />
+  );
+};
+
+export { InputPassword };
