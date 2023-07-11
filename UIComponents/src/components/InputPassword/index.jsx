2eb64fbf801fdd0af6e6305462b5a35e45a3f75e
Carlos Junior Ramos Vásquez <juniorencode@outlook.com>
2023-07-11 00:16:33 -0500

    feat(UIComponents): changed icon

--- UIComponents/src/components/InputPassword/index.jsx
+++ UIComponents/src/components/InputPassword/index.jsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import './InputPassword.css';
 
 const InputPassword = () => {
+  const [hidden, setHidden] = useState(false);
+
+  const handleHidenn = () => {
+    console.log('x');
+    setHidden(!hidden);
+  };
+
   return (
     <div className="InputPassword">
       <input placeholder="Type password..." type="password" />
       <button tabIndex="-1">
-        <span className="icon-eye-blocked"></span>
+        <span
+          className={`${hidden ? 'icon-eye-blocked' : 'icon-eye'}`}
+          onClick={() => handleHidenn()}
+        ></span>
       </button>
     </div>
   );
