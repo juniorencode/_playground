9d5f5cd177cc1694303eb0899cb5d9fba9b74c0a
Carlos Junior Ramos Vasquez <juniorencode@outlook.com>
2024-11-22 15:23:37 -0500

    feat: useEventListener

--- ReactCustomHooks/src/App.jsx
+++ ReactCustomHooks/src/App.jsx
@@ -1,24 +1,13 @@
-import { useArray } from './hooks/useArray.hook';
+import { useState } from 'react';
+import { useEventListener } from './hooks/useEventListener.hook';
 
 function App() {
-  const { array, set, push, insert, remove, filter, update, reverse, clear } =
-    useArray([1, 2, 3, 4, 5, 6]);
+  const [key, setKey] = useState('');
+  useEventListener('keydown', e => {
+    setKey(e.key);
+  });
 
-  return (
-    <div>
-      <div>{array.join(', ')}</div>
-      <button onClick={() => push(7)}>Add 7</button>
-      <button onClick={() => insert(200, 100)}>Add 100</button>
-      <button onClick={() => update(1, 9)}>Change Second Element To 9</button>
-      <button onClick={() => remove(1)}>Remove Second Element</button>
-      <button onClick={() => filter(n => n < 3)}>
-        Keep Numbers Less Than 4
-      </button>
-      <button onClick={() => set([1, 2])}>Set To 1, 2</button>
-      <button onClick={reverse}>Reverse</button>
-      <button onClick={clear}>Clear</button>
-    </div>
-  );
+  return <div>Last Key: {key}</div>;
 }
 
 export default App;
