7fe68562b91683405617b85e36191729983ece4d
Carlos Junior Ramos Vasquez <juniorencode@outlook.com>
2024-11-23 11:12:52 -0500

    feat: useStorage

--- ReactCustomHooks/src/App.jsx
+++ ReactCustomHooks/src/App.jsx
@@ -1,9 +1,20 @@
-import { useMediaQuery } from './hooks/useMediaQuery.hook';
+import { useSessionStorage, useLocalStorage } from './hooks/useStorage.hook';
 
 function App() {
-  const isLarge = useMediaQuery('(min-width: 200px)');
+  const [name, setName, removeName] = useSessionStorage('name', 'Sergey');
+  const [age, setAge, removeAge] = useLocalStorage('age', 26);
 
-  return <div>Large: {isLarge.toString()}</div>;
+  return (
+    <div>
+      <div>
+        {name} - {age}
+      </div>
+      <button onClick={() => setName('John')}>Set Name</button>
+      <button onClick={() => setAge(40)}>Set Age</button>
+      <button onClick={removeName}>Remove Name</button>
+      <button onClick={removeAge}>Remove Age</button>
+    </div>
+  );
 }
 
 export default App;
