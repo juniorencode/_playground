82b0450d7ecf2996f5d7af5c72cb4fdab30099de
Carlos Junior Ramos Vasquez <juniorencode@outlook.com>
2024-11-28 17:55:25 -0500

    feat: utils empty, equal, clone and merge

--- ReactUtilities/src/utilities/utils.js
+++ ReactUtilities/src/utilities/utils.js
@@ -34,6 +34,54 @@ const isDate = value => {
   return isObject(value) && value instanceof Date && !isNaN(value);
 };
 
+const isEmpty = value => {
+  return isObject(value) && Object.keys(value).length === 0;
+};
+
+const isEqual = (a, b) => {
+  if (a === b) return true;
+
+  if (!isObject(a) || !isObject(b)) return false;
+
+  const keysA = Object.keys(a);
+  const keysB = Object.keys(b);
+
+  if (keysA.length !== keysB.length) return false;
+
+  for (let key of keysA) {
+    if (!keysB.includes(key) || !isEqual(a[key], b[key])) return false;
+  }
+
+  return true;
+};
+
+const deepClone = obj => {
+  if (!isObject(obj)) return obj;
+
+  const clone = isArray(obj) ? [] : {};
+
+  for (let key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      clone[key] = isObject(obj[key]) ? deepClone(obj[key]) : obj[key];
+    }
+  }
+
+  return clone;
+};
+
+const deepMerge = (target, source) => {
+  if (!isObject(target) || !isObject(source)) return target;
+
+  for (const key in source) {
+    if (isObject(source[key]) && key in target) {
+      Object.assign(source[key], deepMerge(target[key], source[key]));
+    }
+  }
+
+  Object.assign(target, source);
+  return target;
+};
+
 export {
   isDefined,
   typeOf,
@@ -43,5 +91,9 @@ export {
   isFunction,
   isArray,
   isObject,
-  isDate
+  isDate,
+  isEmpty,
+  isEqual,
+  deepClone,
+  deepMerge
 };
