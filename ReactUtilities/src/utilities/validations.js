b8a417c99b5292b33aafe35f693f93fed25a4184
Carlos Junior Ramos Vasquez <juniorencode@outlook.com>
2024-11-27 19:34:10 -0500

    feat: validations number

--- ReactUtilities/src/utilities/validations.js
+++ ReactUtilities/src/utilities/validations.js
@@ -3,5 +3,20 @@ export const getValidationError = (value, validation) => {
     return { type: 'required', message: 'Este campo es requerido.' };
   }
 
+  if (value && typeof value === 'number') {
+    if (validation.minValue && value < validation.minValue) {
+      return {
+        type: 'minValue',
+        message: `El número debe ser mayor o igual a ${validation.minValue}.`
+      };
+    }
+    if (validation.maxValue && value > validation.maxValue) {
+      return {
+        type: 'maxValue',
+        message: `El número debe ser menor o igual a ${validation.maxValue}.`
+      };
+    }
+  }
+
   return null;
 };
