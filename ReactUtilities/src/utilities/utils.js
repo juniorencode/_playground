c2603c05e064242d774a9c405274763ac8f818ba
Carlos Junior Ramos Vasquez <juniorencode@outlook.com>
2024-11-28 17:51:09 -0500

    feat: utils validation native type

--- ReactUtilities/src/utilities/utils.js
+++ ReactUtilities/src/utilities/utils.js
@@ -6,4 +6,37 @@ const typeOf = (value, type) => {
   return isDefined(value) && typeof value === type;
 };
 
-export { isDefined, typeOf };
+const isNumber = value => {
+  return typeOf(value, 'number');
+};
+
+const isString = value => {
+  return typeOf(value, 'string');
+};
+
+const isBoolean = value => {
+  return typeOf(value, 'boolean');
+};
+
+const isFunction = value => {
+  return typeOf(value, 'function');
+};
+
+const isArray = value => {
+  return Array.isArray(value);
+};
+
+const isObject = value => {
+  return typeOf(value, 'object') && !isFunction(value) && !isArray(value);
+};
+
+export {
+  isDefined,
+  typeOf,
+  isNumber,
+  isString,
+  isBoolean,
+  isFunction,
+  isArray,
+  isObject
+};
