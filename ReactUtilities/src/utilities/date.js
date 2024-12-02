d9c7c9655a8065616a4954563f5541eba9a43f98
Carlos Junior Ramos Vasquez <juniorencode@outlook.com>
2024-12-01 22:31:30 -0500

    feat: constants of names

---
+++ ReactUtilities/src/utilities/date.js
@@ -0,0 +1,41 @@
+export const monthNames = [
+  'Enero',
+  'Febrero',
+  'Marzo',
+  'Abril',
+  'Mayo',
+  'Junio',
+  'Julio',
+  'Agosto',
+  'Septiembre',
+  'Octubre',
+  'Noviembre',
+  'Diciembre'
+];
+
+export const shortMonthNames = [
+  'Ene',
+  'Feb',
+  'Mar',
+  'Abr',
+  'May',
+  'Jun',
+  'Jul',
+  'Ago',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dic'
+];
+
+export const daysOfWeekNames = [
+  'Lunes',
+  'Martes',
+  'Miércoles',
+  'Jueves',
+  'Viernes',
+  'Sábado',
+  'Domingo'
+];
+
+export const shortDaysOfWeekNames = ['Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa', 'Do'];
